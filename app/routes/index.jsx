233a20914e0418ecfdb808c41ed2f125322f97ae
Victor Kane <victorkane@gmail.com>
2022-05-03 05:57:16 -0300

    edits to homepage

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,6 +2,11 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <p>
+        This repo is in javascript for now since Typescript is still new for
+        many people. crash course.
+      </p>
+      <p>This is a simple blog app from the Traversy Media Remix</p>
     </div>
   )
 }
