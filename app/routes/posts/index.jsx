e427f3ab336f9d1c000d825f9ff0f1b297748185
Victor Kane <victorkane@gmail.com>
2022-05-03 10:41:08 -0300

    Initial loader usage with hard-coded posts data prepares for later loading from database

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,20 +1,36 @@
-import { Outlet, useLoaderData } from "@remix-run/react"
+import { Outlet, useLoaderData, Link } from "@remix-run/react"
 
 export const loader = () => {
-  console.log(123)
-  return {}
+  const data = {
+    posts: [
+      { id: 1, title: "Post 1", body: "This is a test post" },
+      { id: 2, title: "Post 2", body: "This is a test post" },
+      { id: 3, title: "Post 3", body: "This is a test post" },
+    ],
+  }
+  return data
 }
 
 export default function PostItems() {
+  const { posts } = useLoaderData()
+  console.log(posts)
   return (
-    <div>
-      <h1>Post Items</h1>
-      <p>
-        I'm the posts route index file nested right here on the posts route page
-        and visible at slash posts.
-      </p>
-      <p>Take a look at slash posts slash new (manually, no link here yet)</p>
-      <Outlet />
-    </div>
+    <>
+      <div className="page-header">
+        <h1>Post Items</h1>
+        <Link to="new" className="btn">
+          New Post
+        </Link>
+      </div>
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.id.toString()}>
+              <h3>{post.id}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
