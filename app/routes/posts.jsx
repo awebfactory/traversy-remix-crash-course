910fa9955fe30a1880a05eadc158c2878e3a178e
Victor Kane <victorkane@gmail.com>
2022-05-03 05:57:44 -0300

    File based routing

--- app/routes/posts.jsx
+++ app/routes/posts.jsx
@@ -1,7 +1,9 @@
 export default function Posts() {
   return (
     <div>
-      <h1>This is the Posts route</h1>
+      <h1>Posts</h1>
+      <h2>This is the File Based Routing file for the posts route</h2>
+      <p>It just works</p>
     </div>
   )
 }
