6bb1801d59b8d0de4b6540fb0963ed6180a9c515
Victor Kane <victorkane@gmail.com>
2022-05-05 10:40:10 -0300

    chore: remove pedagogic text from root post route

--- app/routes/posts.jsx
+++ app/routes/posts.jsx
@@ -2,18 +2,8 @@ import { Outlet } from "@remix-run/react"
 
 export default function Posts() {
   return (
-    <div>
-      <h1>Posts</h1>
-      <h2>This is the File Based Routing file for the posts route</h2>
-      <p>It just works</p>
-      <h2>Nested Routing</h2>
-      <p>
-        Now I can nest routes in the name matching `posts` folder right here on
-        this page at this outlet. Below this comment you can see the nested
-        routes
-      </p>
-
+    <>
       <Outlet />
-    </div>
+    </>
   )
 }
