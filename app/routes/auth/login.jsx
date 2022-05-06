a76c72ca44f3317054860e080d505e9a913030fc
Victor Kane <victorkane@gmail.com>
2022-05-06 12:16:54 -0300

    feat(auth): Added validation for login form

--- app/routes/auth/login.jsx
+++ app/routes/auth/login.jsx
@@ -1,4 +1,43 @@
+import { useActionData } from "@remix-run/react"
+import { redirect, json } from "@remix-run/node"
+import { db } from "~/utils/db.server"
+
+function validateUsername(username) {
+  if (typeof username !== "string" || username.length < 3) {
+    return "Username must be at least 3 characters"
+  }
+}
+
+function validatePassword(password) {
+  if (typeof password !== "string" || password.length < 6) {
+    return "Password must be at least 6 characters"
+  }
+}
+
+function badRequest(data) {
+  return json(data, { status: 400 })
+}
+
+export const action = async ({ request }) => {
+  const form = await request.formData()
+  const loginType = form.get("loginType")
+  const username = form.get("username")
+  const password = form.get("password")
+
+  const fields = { loginType, username, password }
+
+  const fieldErrors = {
+    username: validateUsername(username),
+    password: validatePassword(password),
+  }
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields })
+  }
+}
+
 export default function Login() {
+  const actionData = useActionData()
   return (
     <div className="auth-container">
       <div className="page-header">
@@ -9,23 +48,48 @@ export default function Login() {
           <fieldset>
             <legend>Login or Register</legend>
             <label>
-              <input type="radio" name="loginType" value="login" /> Login
+              <input
+                type="radio"
+                name="loginType"
+                value="login"
+                defaultChecked={
+                  !actionData?.fields?.loginType ||
+                  actionData?.fields?.loginType === "login"
+                }
+              />{" "}
+              Login
             </label>
 
             <label>
-              <input type="radio" name="loginType" value="register" /> Register
+              <input
+                type="radio"
+                name="loginType"
+                value="register"
+                defaultChecked={actionData?.fields?.loginType === "register"}
+              />{" "}
+              Register
             </label>
           </fieldset>
           <div className="form-control">
             <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" />
-            <div className="error"></div>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              defaultValue={actionData?.fields?.username}
+            />
+            <div className="error">{actionData?.fieldErrors?.username}</div>
           </div>
 
           <div className="form-control">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-            <div className="error"></div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              defaultValue={actionData?.fields?.password}
+            />
+            <div className="error">{actionData?.fieldErrors?.password}</div>
           </div>
 
           <button className="btn btn-block" type="submit">
